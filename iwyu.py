438dff231ee8f51cb0fd853f2728c6b1fbcbdcb2
Volodymyr Sapsai <vsapsai@gmail.com>
2014-12-26 17:08:52 -0800

    Initial version.

---
+++ iwyu.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+
+# As an example was taken EC2LatentBuildSlave https://github.com/buildbot/buildbot/blob/master/master/buildbot/buildslave/ec2.py
+
+import os
+import time
+
+try:
+    import boto
+    import boto.ec2
+except ImportError:
+    boto = None
+
+from twisted.internet import threads
+from twisted.python import log
+
+from buildbot import config
+from buildbot.buildslave.base import AbstractLatentBuildSlave
+
+RUNNING = "running"
+STOPPED = "stopped"
+
+class EC2LatentBuildSlave(AbstractLatentBuildSlave):
+    """Slave which starts existing EC2 instance, executes build, stops instance.
+
+    Difference between default EC2LatentBuildSlave is it doesn't create a new
+    instance, but starts existing.
+    """
+
+    _poll_resolution = 5
+
+    def __init__(self, name, password,
+                 aws_id_file_path, region, instance_name):
+        if not boto:
+            config.error("The python module 'boto' is needed to use EC2 build slaves")
+        AbstractLatentBuildSlave.__init__(self, name, password,
+            max_builds=None, notify_on_missing=[], missing_timeout=60 * 20,
+            build_wait_timeout=0, properties={}, locks=None)
+        if not os.path.exists(aws_id_file_path):
+            raise ValueError(
+                "Please supply your AWS credentials in "
+                "the {} file (on two lines).".format(aws_id_file_path))
+        with open(aws_id_file_path, "r") as aws_credentials_file:
+            access_key_id = aws_credentials_file.readline().strip()
+            secret_key = aws_credentials_file.readline().strip()
+        self.conn = boto.ec2.connect_to_region(region,
+            aws_access_key_id=access_key_id, aws_secret_access_key=secret_key)
+        self.instance = self.conn.get_only_instances(filters={"tag:Name": instance_name})[0]
+
+    def start_instance(self, build):
+        return threads.deferToThread(self._start_instance)
+
+    def _start_instance(self):
+        if self.instance.state != RUNNING:
+            self.instance.start()
+        while self.instance.state != RUNNING:
+            time.sleep(self._poll_resolution)
+            self.instance.update()
+        log.msg("Started instance")
+        return True
+
+    def stop_instance(self, fast=False):
+        return threads.deferToThread(self._stop_instance)
+
+    def _stop_instance(self):
+        if self.instance.state != STOPPED:
+            self.instance.stop()
+        while self.instance.state != STOPPED:
+            time.sleep(self._poll_resolution)
+            self.instance.update()
+        log.msg("Stopped instance")
---
+++ provisioning_slave.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y subversion git
+sudo apt-get install -y build-essential ncurses-dev libz-dev
+sudo apt-get install -y cmake
+
+# Install ninja.
+git clone git://github.com/martine/ninja.git ~/ninja
+cd ~/ninja
+./configure.py --bootstrap
+sudo cp ./ninja /usr/bin/ninja
+
+# Install buildbot slave.
+sudo apt-get install -y python-dev python-setuptools
+sudo easy_install buildbot-slave
+buildslave create-slave slave buildbot-master.include-what-you-use.com:9989 <name> <password>
+
+
+# Setup EBS.
+sudo mkfs -t ext4 /dev/xvdf
+sudo mkdir /mnt/buildbot_iwyu_trunk
+sudo mount /dev/xvdf /mnt/buildbot_iwyu_trunk/
+sudo mkdir /mnt/buildbot_iwyu_trunk/workspace
+sudo chown ubuntu:ubuntu /mnt/buildbot_iwyu_trunk/workspace/
+
+mkdir /mnt/buildbot_iwyu_trunk/workspace/sources
+svn checkout http://llvm.org/svn/llvm-project/llvm/trunk /mnt/buildbot_iwyu_trunk/workspace/sources/llvm
+svn checkout http://llvm.org/svn/llvm-project/cfe/trunk /mnt/buildbot_iwyu_trunk/workspace/sources/llvm/tools/clang
+
+mkdir /mnt/buildbot_iwyu_trunk/workspace/build_llvm
+cd /mnt/buildbot_iwyu_trunk/workspace/build_llvm
+cmake -G Ninja -DCMAKE_INSTALL_PREFIX=/mnt/buildbot_iwyu_trunk/workspace/installed/ /mnt/buildbot_iwyu_trunk/workspace/sources/llvm/
+ninja install
+
+
+svn checkout https://include-what-you-use.googlecode.com/svn/trunk iwyu
+cmake -G Ninja -DLLVM_PATH=/mnt/buildbot_iwyu_trunk/workspace/installed/ /mnt/buildbot_iwyu_trunk/workspace/sources/iwyu/
